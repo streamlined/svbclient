e166fe14f4dd7e8bf7bc2a17c679aa5d6af57c08
boris <boris@streamlinedpayments.com>
2025-06-26 20:53:55 -0700

    Bump version. Update method for reversal.

--- lib/api/ach.rb
+++ lib/api/ach.rb
@@ -99,10 +99,13 @@ module ACH
       batch_details: {
         settlement_priority: "STANDARD"
       },
-      reversal_details_single_mass_id: {
-        reason_for_reversal: reason,
-        original_mass_delivery_id: id
-      }
+      reversal_details_single_mass_id: {},
+      reversal_details_multiple_id: [
+        {
+          reason_for_reversal: reason,
+          original_id: id
+        }
+      ]
     }
 
     headers = {
